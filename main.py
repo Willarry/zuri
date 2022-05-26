b4255d3f29701a5b658d9ae6193dc9ece114d92c
William Larry <77531807+Willarry@users.noreply.github.com>
2022-05-26 17:39:03 +0100

    Add finding anagrams

---
+++ main.py
@@ -0,0 +1,9 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    return sorted(word) == sorted(anagram)
+
