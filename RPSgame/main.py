d269e82b0eceb8a1d21c0a7eac4efa32cb8a16e8
William Larry <77531807+Willarry@users.noreply.github.com>
2022-06-11 15:41:50 +0100

    Add files via upload

---
+++ RPSgame/main.py
@@ -0,0 +1,43 @@
+import random
+
+choices = ["rock", "paper", "scissors"]
+computer = random.choice(choices)
+player = None
+
+while player not in choices:
+ player = input("rock, paper or scissors?:").lower()
+ 
+if player == computer:
+    print("computer: ",computer)
+    print("player: ",player)
+    print("Tie!")
+elif player == "rock":
+    if computer == "paper":
+        print ("computer: ",computer)
+        print ("player:",player)
+        print("You lose!")
+    if computer == "scissors":
+        print ("computer: ",computer)
+        print ("player:",player)
+        print("You win!")    
+        
+elif player == "scissors":
+    if computer == "rock":
+        print ("computer: ",computer)
+        print ("player:",player)
+        print("You lose!")
+    if computer == "paper":
+        print ("computer: ",computer)
+        print ("player:",player)
+        print("You win!")    
+        
+elif player == "paper":
+    if computer == "scissors":
+        print ("computer: ",computer)
+        print ("player:",player)
+        print("You lose!")
+    if computer == "rock":
+        print ("computer: ",computer)
+        print ("player:",player)
+        print("You win!")    
+        
